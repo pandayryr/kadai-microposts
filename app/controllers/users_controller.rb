4aafc236f738213dd4699ec8c85fd125a4013150
kouhei.azuse <knp.panda@gmail.com>
2019-12-24 12:29:17 +0000

    follow, unfollow

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :require_user_logged_in, only: [:index, :show]
+  before_action :require_user_logged_in, only: [:index, :show, :edit, :update, :destroy, :followings, :followers]
   
   def index
     @users = User.order(id: :desc).page(params[:page]).per(10)
@@ -50,15 +50,26 @@ class UsersController < ApplicationController
     redirect_to users_url
   end
   
-end
+  def followings
+    @user = User.find(params[:id])
+    @followings = @user.followings.page(params[:page])
+    counts(@user)
+  end
+  
+  def followers
+    @user = User.find(params[:id])
+    @followers = @user.followers.page(params[:page])
+    counts(@user)
+  end
+  
+  private
 
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 
-private
-
-def user_params
-  params.require(:user).permit(:name, :email, :password, :password_confirmation)
-end
-
-def user_name_params
-  params.require(:user).permit(:name)
+  def user_name_params
+    params.require(:user).permit(:name)
+  end
+  
 end
